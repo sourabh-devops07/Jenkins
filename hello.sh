8d8bffce74ff746d61c7b730d10589016dfe5533
sourabh-devops07 <sourabhsharma0929@gmail.com>
2026-07-09 17:37:20 +0530

    Update script to install nginx after system update
    Change update message and install nginx after updating.

--- hello.sh
+++ hello.sh
@@ -3,4 +3,7 @@ echo "User : $(whoami)"
 echo "Hostname : $(hostname)"
 echo "Current Directory : $(pwd)"
 echo "Date : $(date)"
-echo "Update : $(sudo apt update)"
+echo "Installing Updates"
+sudo apt update
+sudo apt install -y nginx
+nginx --version
